cb6818bf41e4016a6c8a7764d0cce896d559409e
jadj <jakub.jadczyk@innsoft.pl>
2024-09-29 12:01:54 +0200

    replace toggle from custom  to shadcn

--- frontend/src/layouts/EventsSearch/EventsSearch.tsx
+++ frontend/src/layouts/EventsSearch/EventsSearch.tsx
@@ -1,10 +1,12 @@
 
 import { useState } from "react";
 import { Input } from "@/components/ui/input/input";
-import { Root as ToogleGroup, Item as ToogleItem } from '@radix-ui/react-toggle-group';
-import styles from "./EventsSearch.module.css";
-import { cn } from "@/lib/utils";
 import { EventSearchCriteria } from "@/hooks/useEventsSearch";
+import {
+    ToggleGroup,
+    ToggleGroupItem,
+} from "@/components/ui/toggle-group"
+
 
 interface EventsSearchProps {
     setSearchValue: React.Dispatch<React.SetStateAction<string>>;
@@ -27,18 +29,17 @@ export function EventsSearch({ setSearchValue, setSearchBy }: EventsSearchProps)
         <div className="grid  p-2 gap-4 xl:grid-cols-2">
             <div className="flex gap-2 items-center">
                 <span>Search by</span>
-                <ToogleGroup
-                    className={cn(styles.ToggleGroup)}
-                    type="single"
-                    defaultValue="all"
-                    aria-label="Text alignment"
-                    onValueChange={handleSearchByValue}
-                    value={toggleGroupValue}
-                >
-                    <ToogleItem value="eventName" aria-label="Search by name" className={cn(styles.ToggleGroupItem)}> <p> name</p></ToogleItem>
-                    <ToogleItem value="eventDetails" aria-label="Search by details" className={cn(styles.ToggleGroupItem)}> details</ToogleItem>
-                    <ToogleItem value="all" aria-label="Search by all" className={cn(styles.ToggleGroupItem)}> all</ToogleItem>
-                </ToogleGroup>
+                <ToggleGroup type="single" onValueChange={handleSearchByValue} defaultValue="all" value={toggleGroupValue}>
+                    <ToggleGroupItem value="eventName" aria-label="Search by name">
+                        name
+                    </ToggleGroupItem>
+                    <ToggleGroupItem value="eventDetails" aria-label="Search by details">
+                        details
+                    </ToggleGroupItem>
+                    <ToggleGroupItem value="all" aria-label="Search by all">
+                        all
+                    </ToggleGroupItem>
+                </ToggleGroup>
             </div>
 
             <Input

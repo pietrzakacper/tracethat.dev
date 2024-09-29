412686b0cefdc0059adec0f628bfcd3654908215
jadj <jakub.jadczyk@innsoft.pl>
2024-09-29 11:19:12 +0200

    change useEffect into useMemo

--- frontend/src/hooks/useEventsSearch.ts
+++ frontend/src/hooks/useEventsSearch.ts
@@ -1,44 +1,35 @@
-import { useEffect, useMemo, useState } from "react"
+import { useMemo } from "react";
 import { TraceEvent } from "@/validators/TraceEvent";
 
 interface UseEventsSearchArguments {
-    searchValue: string,
-    searchBy: EventSearchCriteria
-    data: TraceEvent[]
+    searchValue: string;
+    searchBy: EventSearchCriteria;
+    data: TraceEvent[];
 }
 
 export type EventSearchCriteria = 'eventName' | 'eventDetails' | 'all';
 
 export const useEventsSearch = ({ searchValue, data, searchBy }: UseEventsSearchArguments) => {
-    const [filteredData, setFilteredData] = useState<TraceEvent[] | null>();
-
-    //tu use memo i zwracac filtrowane dane 
-
-    useEffect(() => {
-
-        if (searchValue) {
-
-            const filteredData = data.filter((item) => {
-
-                if (searchBy === "eventName") {
-                    return item.name.toLowerCase().includes(searchValue.toLowerCase());
-                } else if (searchBy === "eventDetails") {
-
-
-                    return JSON.stringify(item.details).toLowerCase().includes(searchValue.toLowerCase());
-                } else {
-
-                    return item.name.toLowerCase().includes(searchValue.toLowerCase()) || JSON.stringify(item.details).toLowerCase().includes(searchValue.toLowerCase());
-                }
-            })
-
-            setFilteredData(filteredData as TraceEvent[]);
-
-        } else {
-            setFilteredData(null)
+    const filteredData = useMemo(() => {
+        if (!searchValue) {
+            return null;
         }
 
+        return data.filter((item) => {
+            const lowerCaseSearchValue = searchValue.toLowerCase();
+
+            if (searchBy === "eventName") {
+                return item.name.toLowerCase().includes(lowerCaseSearchValue);
+            } else if (searchBy === "eventDetails") {
+                return JSON.stringify(item.details).toLowerCase().includes(lowerCaseSearchValue);
+            } else {
+                return (
+                    item.name.toLowerCase().includes(lowerCaseSearchValue) ||
+                    JSON.stringify(item.details).toLowerCase().includes(lowerCaseSearchValue)
+                );
+            }
+        });
     }, [searchValue, searchBy, data]);
 
-    return { filteredData }
-}
+    return { filteredData };
+};
